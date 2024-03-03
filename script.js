99020ffb5a1a7b5f5d1752e2b90ff43c492698c0
shanmuga priya <rajachandramohan.m@gmail.com>
2024-03-03 21:21:05 +0530

    json file loading

---
+++ script.js
@@ -0,0 +1,20 @@
+let http = new XMLHttpRequest();
+http.open('get','products.json',true);
+http.send();
+http.onload=function(){
+    if(this.readyState==4 && this.status==200){
+        let products=JSON.parse(this.responseText);
+
+        let output="";
+
+        for(let item of products){
+            output +=`
+            <div class="product">
+                <img src="${item.image}" alt="${item.image}">
+                <p class="para">${item.para}</p>
+            </div>
+            `;
+        }
+        document.querySelector(".products").innerHTML=output;
+    }
+}
